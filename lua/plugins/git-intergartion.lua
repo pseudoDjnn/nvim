9ab63dc0845a2499144ce2342ae96be40f385b8d
pseudoDjnn <alexander.havers13@gmail.com>
2024-02-24 22:29:30 -0500

    added gitsigns/fugitive
    run git in the cmd before running it once neovim instances is over.
    Ease of access for VSCode also.

---
+++ lua/plugins/git-intergartion.lua
@@ -0,0 +1,16 @@
+return {
+  --Fugitive
+  {
+    "tpope/vim-fugitive",
+  },
+  --Git Signs for better staging
+  {
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup()
+
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+  end
+  },
+}
