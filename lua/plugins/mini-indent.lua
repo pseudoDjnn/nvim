ae61b69a765a556574d0b36ce82a2387c899bb44
pseudoDjnn <alexander.havers13@gmail.com>
2024-02-29 01:43:23 -0500

    mini-indent
    switched to mini-indentscope
    got rid of blankline

---
+++ lua/plugins/mini-indent.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.indentscope",
+  version = false,
+  config = function()
+    require("mini.indentscope").setup({
+      draw = {
+        delay = 100,
+      }
+    })
+  end,
+}
