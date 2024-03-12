6d27f96acd60937f260cf0a921f5cb213cce8d63
pseudoDjnn <alexander.havers13@gmail.com>
2024-03-12 18:51:41 -0400

    stopped trouble.nvim
    I just didn't see a difference

--- lua/after/plugins/trouble.lua
+++ lua/after/plugins/trouble.lua
@@ -1,13 +1,14 @@
 return {
-  "folke/lsp-colors.nvim",
-  "folke/trouble.nvim",
-  config = function()
-    require("lsp-colors").setup({
-      Error = "#db4b4b",
-      Warning = "#e0af68",
-      Information = "#0db9d7",
-      Hint = "#10B981",
-    })
-    require("trouble").setup()
-  end,
+	"folke/lsp-colors.nvim",
+	"folke/trouble.nvim",
+	lazy = true,
+	config = function()
+		require("lsp-colors").setup({
+			Error = "#db4b4b",
+			Warning = "#e0af68",
+			Information = "#0db9d7",
+			Hint = "#10B981",
+		})
+		require("trouble").setup()
+	end,
 }
